45a36003b5c3a221a3a21e2bcbbecd8402bfe750
Dmytro Soroka <d40a@Dmytros-MacBook-Pro.local>
2017-02-05 23:29:48 -0500

    first commit

---
+++ app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { AppComponent }  from './app.component';
+import { HeroDetailsComponent } from './hero_details.component'
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+  ],
+  declarations: [ 
+    AppComponent,
+    HeroDetailsComponent,
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
